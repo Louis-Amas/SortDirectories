5535c7816c5560986a0413fb9455c04d2f7b671c
Louis-Amas <amaslouis@protonmail.com>
2020-07-02 15:16:39 +0200

    Fix naming

--- .choose_keyboard.sh
+++
@@ -1,8 +0,0 @@
-choice=$(echo -e "us_qwerty-fr \nfr" | dmenu -i -p "Please type the keymap you wish to use") || exit 1
-if [ -z $choice ]; then
-    setxkbmap fr
-
-
-else
-    setxkbmap $choice
-fi
--- .shutdown.sh
+++
@@ -1,3 +0,0 @@
-choice=$(echo -e "suspend\nhibernate\nreboot\npoweroff" | dmenu -i -p "Please type the action to use" || exit 1)
-
-systemctl $choice
